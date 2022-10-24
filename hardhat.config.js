11ab27bb39b50899c48108d10f5093141d33c794
tashun <tashun.chamara@gmail.com>
2022-10-24 16:26:43 +0530

    config

---
+++ hardhat.config.js
@@ -0,0 +1,63 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+//
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      gas: 2500000,
+      gasPrice: 8000000000
+    },
+    // matic: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [process.env.PRIVATE_KEY],
+    //   gas: 2100000
+    // },
+    // mumbai: {
+    //   url: process.env.STAGING_ALCHEMY_KEY,
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    //   gas: 2100000,
+    //   gasPrice: 8000000000
+    // },
+    // rinkeby: {
+    //   url: process.env.STAGING_ALCHEMY_KEY,
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    //   gas: 2100000,
+    //   gasPrice: 8000000000
+    // },
+    ropston: {
+      url: process.env.ROPSTON_RPC,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gas: 2100000,
+      gasPrice: 8000000000
+    },
+
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        }
+      }
+    ]
+  },
+};
