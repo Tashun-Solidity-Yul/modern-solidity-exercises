10afdcbb40b93bc6b8e3a2b6f444cd55fcec041d
tashun <tashun.chamara@gmail.com>
2022-11-30 23:36:05 +0530

    way to create pure yul contract

---
+++ contracts/exercises/Calls/4/i_interface.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+interface  IInterface {
+    function get2() external view returns(uint256);
+    function get99(uint256) external view returns(uint256);
+}
---
+++ contracts/exercises/Calls/4/solidity_contract.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "./i_interface.sol";
+
+contract solidityContract is IInterface {
+    function get2() external override view returns(uint256){
+        return 2;
+    }
+    function get99(uint256 s) external override view returns(uint256) {
+        return 99;
+    }
